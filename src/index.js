ea3de06ca74928fbf7c890a343f141d17e0a6e34
tarunmandhan <mandhantarun@gmail.com>
2022-06-09 23:30:33 +0530

    uploading

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const name = "Tarun Mandhan";
+const dt = new Date().toLocaleDateString();
+const tm = new Date().toLocaleTimeString();
+ReactDOM.render(
+  <>
+    <h1> My name is {name} </h1>
+    <p> The current Date is = {dt} </p>
+    <p> The current Time is = {tm} </p>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
